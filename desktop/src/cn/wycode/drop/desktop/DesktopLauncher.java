4dad2f8240f0ede59949784d30ea5d4456be5a37
wangyu <www_wayne_001@163.com>
2016-05-04 14:34:41 +0800

    Initial commit

---
+++ desktop/src/cn/wycode/drop/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cn.wycode.drop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cn.wycode.drop.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
